b6d055115bf677ec3afa86e25f6de704fb7e6fe3
Karen Melo <karen_melo@live.com>
2022-03-27 23:40:19 -0300

    Implementação do domain da parte de vendas de pedido.

---
+++ src/NerdStore.Vendas.Domain/Entities/Pedido.cs
@@ -0,0 +1,80 @@
+﻿using NerdStore.Core.DomainObjects;
+using NerdStore.Vendas.Domain.Enums;
+
+namespace NerdStore.Vendas.Domain.Entities
+{
+    public class Pedido : Entity, IAggregateRoot
+    {
+        public int Codigo { get; private set; }
+        public Guid ClientId { get; private set; }
+        public Guid? VoucherId { get; private set; }
+        public bool VoucherUtilizado { get; private set; }
+        public decimal Desconto { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
+
+        private readonly List<PedidoItem> _pedidoItens;
+
+        public Pedido(Guid clientId, bool voucherUtilizado, decimal desconto, decimal valorTotal)
+        {
+            ClientId = clientId;
+            VoucherUtilizado = voucherUtilizado;
+            Desconto = desconto;
+            ValorTotal = valorTotal;
+            _pedidoItens = new List<PedidoItem>();
+        }
+
+        public Pedido()
+        {
+            _pedidoItens = new List<PedidoItem>();
+        }
+
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItens;
+
+        //EF Relational
+        public virtual Voucher Voucher { get; private set; }
+
+
+        public void CalcularValorTotalDesconto()
+        {
+            if (!VoucherUtilizado) return;
+
+            decimal desconto = 0;
+            var valor = ValorTotal;
+
+            if (Voucher.TipoDescontoVoucher == TipoDescontoVoucher.Porcentagem)
+            {
+                if (Voucher.Percentual.HasValue)
+                {
+                    desconto = (valor * Voucher.Percentual.Value) / 100;
+                    valor -= desconto;
+                }
+            }
+            else
+            {
+                if (Voucher.ValorDesconto.HasValue)
+                {
+                    desconto = Voucher.ValorDesconto.Value;
+                    valor -= desconto;
+                }
+            }
+
+            ValorTotal = valor < 0 ? 0 : valor;
+            Desconto = desconto;
+        }
+
+        public void CalcularValorPedido()
+        {
+            ValorTotal = PedidoItems.Sum(p => p.CalcularValor());
+            CalcularValorTotalDesconto();
+        }
+
+        public bool PedidoItemExistente(PedidoItem item)
+        {
+            return _pedidoItens.Any(p => p.ProdutoId == item.ProdutoId);
+        }
+
+
+    }
+}
---
+++ src/NerdStore.Vendas.Domain/Entities/PedidoItem.cs
@@ -0,0 +1,54 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Domain.Entities
+{
+    public class PedidoItem : Entity
+    {
+        public PedidoItem(Guid produtoId, string protudoNome, int quantidade, decimal valorUnitario)
+        {
+            ProdutoId = produtoId;
+            ProtudoNome = protudoNome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+
+        protected PedidoItem() { }
+
+        public Guid PedidoId { get; private set; }
+        public Guid ProdutoId { get; private set; }
+        public string ProtudoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+
+        //EF Relational
+        public Pedido Pedido { get; private set; }
+
+
+
+        internal void AssociarPedido(Guid pedidoId)
+        {
+            ProdutoId = pedidoId;
+        }
+
+        public decimal CalcularValor()
+        {
+            return Quantidade * ValorUnitario;
+        }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade += unidades;
+        }
+
+        internal void AtualizarUnidades(int unidades)
+        {
+            Quantidade = unidades;
+        }
+
+    }
+}
